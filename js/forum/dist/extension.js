851c99686f50e08b6b99948d93cdd5a7b98e6d46
blurkness <blurkness@gmail.com>
2016-07-18 20:07:13 -0300

    Fix na pag do user

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -17,38 +17,6 @@ System.register('beeta-dev/ext-userinfo/main', ['flarum/extend', 'flarum/app', '
         execute: function () {
 
             app.initializers.add('beeta-dev-ext-userinfo', function () {
-                extend(UserPage.prototype, 'init', function () {
-                    var user = this.props.user;
-                    var date = user.joinTime();
-                    var hours = date.getHours();
-                    var minutes = date.getMinutes();
-                    var ampm = hours >= 12 ? 'pm' : 'am';
-                    hours = hours % 12;
-                    hours = hours ? hours : 12; // the hour '0' should be '12'
-                    minutes = minutes < 10 ? '0' + minutes : minutes;
-                    var strTime = hours + ':' + minutes + ' ' + ampm;
-                    var df = new Date(date.getMonth() + 1 + "/" + (date.getDate() + 1) + "/" + date.getFullYear() + "  " + strTime);
-                    var dt = new Date();
-                    var allMonths = dt.getMonth() - df.getMonth() + 12 * (dt.getFullYear() - df.getFullYear());
-                    var allYears = dt.getFullYear() - df.getFullYear();
-                    var partialMonths = dt.getMonth() - df.getMonth();
-                    if (partialMonths < 0) {
-                        allYears--;
-                        partialMonths = partialMonths + 12;
-                    }
-                    var goku = "";
-                    var i = "";
-                    for (i = 0; i < allYears; i++) {
-                        goku += "<img src='http://www.beeta.com.br/forum/img/rank/goku.png' />";
-                    }
-                    var esfera = "";
-                    for (i = 0; i < partialMonths; i++) {
-                        esfera += "<img src='http://www.beeta.com.br/forum/img/rank/esfera.gif' />";
-                    }
-                    setTimeout(function () {
-                        $(".UserHero .UserCard-info").append("<div class='daterank'>" + goku + "<br/>" + esfera + "</div>");
-                    }, 100);
-                });
                 extend(UserCard.prototype, 'init', function () {
                     var user = this.props.user;
                     var date = user.joinTime();
@@ -80,6 +48,11 @@ System.register('beeta-dev/ext-userinfo/main', ['flarum/extend', 'flarum/app', '
                     setTimeout(function () {
                         $(".UserCard--popover .UserCard-info").append("<div class='daterank'>" + goku + "<br/>" + esfera + "</div>");
                     }, 100);
+                    if ($(".daterankhero").html() == undefined) {
+                        setTimeout(function () {
+                            $(".UserHero .UserCard-info").append("<div class='daterank daterankhero'>" + goku + "<br/>" + esfera + "</div>");
+                        }, 100);
+                    }
                 });
                 extend(UserCard.prototype, 'infoItems', function (items) {
                     var user = this.props.user;
