980b6daf05a369ac7b6b8dc40648431126d3dafa
blurkness <blurkness@gmail.com>
2016-07-19 20:15:45 -0300

    Fix admin info

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -19,30 +19,38 @@ System.register('beeta-dev/ext-userinfo/main', ['flarum/extend', 'flarum/app', '
             app.initializers.add('beeta-dev-ext-userinfo', function () {
                 extend(UserCard.prototype, 'init', function () {
                     var user = this.props.user;
-                    var date = user.joinTime();
-                    var hours = date.getHours();
-                    var minutes = date.getMinutes();
-                    var ampm = hours >= 12 ? 'pm' : 'am';
-                    hours = hours % 12;
-                    hours = hours ? hours : 12; // the hour '0' should be '12'
-                    minutes = minutes < 10 ? '0' + minutes : minutes;
-                    var strTime = hours + ':' + minutes + ' ' + ampm;
-                    var df = new Date(date.getMonth() + 1 + "/" + (date.getDate() + 1) + "/" + date.getFullYear() + "  " + strTime);
-                    var dt = new Date();
-                    var allMonths = dt.getMonth() - df.getMonth() + 12 * (dt.getFullYear() - df.getFullYear());
-                    var allYears = dt.getFullYear() - df.getFullYear();
-                    var partialMonths = dt.getMonth() - df.getMonth();
-                    if (partialMonths < 0) {
-                        allYears--;
-                        partialMonths = partialMonths + 12;
+                    // Calc
+                    var todate = new Date();
+                    var age = [],
+                        fromdate = new Date(user.joinTime()),
+                        y = [todate.getFullYear(), fromdate.getFullYear()],
+                        ydiff = y[0] - y[1],
+                        m = [todate.getMonth(), fromdate.getMonth()],
+                        mdiff = m[0] - m[1],
+                        d = [todate.getDate(), fromdate.getDate()],
+                        ddiff = d[0] - d[1];
+
+                    if (mdiff < 0 || mdiff === 0 && ddiff < 0) --ydiff;
+                    if (mdiff < 0) mdiff += 12;
+                    if (ddiff < 0) {
+                        fromdate.setMonth(m[1] + 1, 0);
+                        ddiff = fromdate.getDate() - d[1] + d[0];
+                        --mdiff;
                     }
+                    /*
+                    if(ydiff> 0) age.push(ydiff+ ' year'+(ydiff> 1? 's ':' '));
+                    if(mdiff> 0) age.push(mdiff+ ' month'+(mdiff> 1? 's':''));
+                    if(ddiff> 0) age.push(ddiff+ ' day'+(ddiff> 1? 's':''));
+                    if(age.length>1) age.splice(age.length-1,0,' and ');    
+                    return age.join('');
+                    */
                     var goku = "";
                     var i = "";
-                    for (i = 0; i < allYears; i++) {
+                    for (i = 0; i < ydiff; i++) {
                         goku += "<img src='http://www.beeta.com.br/forum/img/rank/goku.png' />";
                     }
                     var esfera = "";
-                    for (i = 0; i < partialMonths; i++) {
+                    for (i = 0; i < mdiff; i++) {
                         esfera += "<img src='http://www.beeta.com.br/forum/img/rank/esfera.gif' />";
                     }
                     setTimeout(function () {
